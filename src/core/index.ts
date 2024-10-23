2a4c9dbb9acdd52913255451f354d552e5116a50
Adrian <38627070+adrianderstroff@users.noreply.github.com>
2024-10-23 21:45:01 +0200

    added missing classes to package index files

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,8 @@
 export * from './buffer';
 export * from './framebuffer';
+export * from './genericbuffer';
 export * from './shader';
 export * from './texture';
+export * from './vertexarrayobject';
 export * from './webgl';
+
--- src/math/index.ts
+++ src/math/index.ts
@@ -1,4 +1,5 @@
 export * from './mat';
 export * from './scalar';
 export * from './transform';
+export * from './type';
 export * from './vec';
